afcb28b35031943c5377b3fdcbf0fd0c6951ff64
Ulrica-Stark <67767333+Ulrica-Stark@users.noreply.github.com>
2026-03-08 11:41:16 +0800

    Update requires.test.js

--- tests/requires.test.js
+++ tests/requires.test.js
@@ -5,6 +5,7 @@ const readdirRecursively = (dir) => {
   const dirents = fs.readdirSync(dir, { withFileTypes: true });
   let files = [];
   let dirs = [];
+  
   dirents.forEach((dirent) => {
     if (dirent.isDirectory()) dirs.push(`${dir}/${dirent.name}`);
     if (dirent.isFile()) files.push(`${dir}/${dirent.name}`);
