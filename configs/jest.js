f0ac1b6dd3503a681eddb176f3265090b738bc6e
Ulrica-Stark <67767333+Ulrica-Stark@users.noreply.github.com>
2026-03-11 19:06:55 +0800

    Update jest.js

--- configs/jest.js
+++ configs/jest.js
@@ -8,6 +8,7 @@ module.exports = {
       extends: [
         // https://github.com/jest-community/eslint-plugin-jest/blob/9f4f669bd238e2ede33f13b087d27ff81a444d64/src/index.ts
         'plugin:jest/recommended',
+        
       ],
       plugins: ['jest'],
     },
